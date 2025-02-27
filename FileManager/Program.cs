603be3231e67af85df6bdbf2f0fb4e00bc546a9f
kdevch1k <sb105005@gmail.com>
2025-02-27 12:41:58 +0300

    Initial commit

---
+++ FileManager/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var app = new FileManagerApp();
+        app.Run();
+    }
+}
+public class FileManagerApp
+{
+    public FileManagerApp()
+    {
+
+    }
+    
+    public void Run()
+    {
+
+    }
+}
